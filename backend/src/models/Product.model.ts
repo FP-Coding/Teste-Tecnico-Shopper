7820076a925bbd176bc7e0678e287ef7bed04d78
Felipe Pinto <felipe14062000@gmail.com>
2023-09-05 19:35:28 -0300

    feat: criação da função create do ProductModel

--- backend/src/models/Product.model.ts
+++ backend/src/models/Product.model.ts
@@ -1,16 +1,26 @@
+import 'dotenv/config';
+import connection from './connection';
 import IProduct from '../interfaces/IProduct';
 import { SimpleModel } from '../interfaces/IModel';
-import connection from './connection';
+import getPlaceholdersAndColumns from '../utils/getPlaceholdersAndColumns';
+import { ResultCreate } from '../interfaces/InterfacesMysql';
+
+
+const DB = process.env.MYSQL_DATABASE; 
 
 class ProductModel implements SimpleModel<IProduct> {
-	create(obj: IProduct): Promise<IProduct> {
+	async create(product: IProduct): Promise<IProduct> {
+		const { placeholders, columns } = getPlaceholdersAndColumns<IProduct>(product);
+		const query = `INSERT INTO ${DB}.products (${columns}) VALUES (${placeholders})`;
+		const [{ insertId }]: ResultCreate = await connection.execute(query, Object.values(product));
+		return { code: insertId, ...product };
+	}
+	async list(): Promise<IProduct[]> {
 		throw new Error('Method not implemented.');
 	}
-	list(): Promise<IProduct[]> {
+	async find(_id: number): Promise<IProduct | null> {
 		throw new Error('Method not implemented.');
 	}
-	find(id: number): Promise<IProduct | null> {
-		throw new Error('Method not implemented.');
-	}
-
 }
+
+export default ProductModel;
