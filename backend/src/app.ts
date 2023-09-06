617a14f725ac27e1b8c994214e0cd1f5f72745bc
Felipe Pinto <felipe14062000@gmail.com>
2023-09-05 21:38:37 -0300

    feat: utilização do ProductRouter no app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import 'dotenv/config';
+import ProductRouter from './routes/Product.route';
 
 const app = express();
 
 app.use(express.json());
+app.use('/product', ProductRouter);
 
 export default app;
