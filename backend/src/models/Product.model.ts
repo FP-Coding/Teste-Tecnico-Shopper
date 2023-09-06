0ea24a8db54f0f56b269fe2de6cb26c59fee2532
Felipe Pinto <felipe14062000@gmail.com>
2023-09-05 21:36:38 -0300

    feat: adição de construtor na classe ProductModel

--- backend/src/models/Product.model.ts
+++ backend/src/models/Product.model.ts
@@ -1,18 +1,19 @@
 import 'dotenv/config';
 import connection from './connection';
 import IProduct from '../interfaces/IProduct';
+import { ResultCreate } from '../interfaces/InterfacesMysql';
 import { SimpleModel } from '../interfaces/IModel';
 import getPlaceholdersAndColumns from '../utils/getPlaceholdersAndColumns';
-import { ResultCreate } from '../interfaces/InterfacesMysql';
-
 
 const DB = process.env.MYSQL_DATABASE; 
 
 class ProductModel implements SimpleModel<IProduct> {
+	constructor(private conn = connection) {}
+
 	async create(product: IProduct): Promise<IProduct> {
 		const { placeholders, columns } = getPlaceholdersAndColumns<IProduct>(product);
 		const query = `INSERT INTO ${DB}.products (${columns}) VALUES (${placeholders})`;
-		const [{ insertId }]: ResultCreate = await connection.execute(query, Object.values(product));
+		const [{ insertId }]: ResultCreate = await this.conn.execute(query, Object.values(product));
 		return { code: insertId, ...product };
 	}
 	async list(): Promise<IProduct[]> {
