1e8082e201eeccfbd1fd03d22dd576672992d1a2
Felipe Pinto <felipe14062000@gmail.com>
2023-09-06 16:25:43 -0300

    refactoring: remoção de conexão do banco através do mysql2 para implementação do sequelize

--- backend/src/routes/Product.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import ProductController from '../controllers/Product.controller';
-
-const baseController = new ProductController();
-const router = Router();
-
-router.post('/', baseController.create);
-
-export default Router;
