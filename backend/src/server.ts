a9f92b4e4e324279efc27e9f99f2daf92b55030e
Felipe Pinto <felipe14062000@gmail.com>
2023-09-06 16:33:42 -0300

    fix: conversão da variável de ambiente de string para number

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT = process.env.PORT;
+const PORT = Number(process.env.PORT);
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Escutando na porta ${PORT}`));
