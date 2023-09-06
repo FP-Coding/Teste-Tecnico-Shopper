caf5331ff8272f05bf2acbfb05948625d381f90b
Felipe Pinto <felipe14062000@gmail.com>
2023-09-06 16:36:57 -0300

    feat: implementação das rotas get, post /product e get /product/:id

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'dotenv/config';
-import ProductRouter from './routes/Product.route';
+import ProductController from './controllers/Product.controller';
 
 const app = express();
 
 app.use(express.json());
-app.use('/product', ProductRouter);
+
+app.get('/product/:id', (req: Request, res: Response, next: NextFunction) => ProductController.find(req, res, next)); 
+app.get('/product', (req: Request, res: Response, next: NextFunction) => ProductController.list(req, res, next)); 
+app.post('/product', (req: Request, res: Response, next: NextFunction) => ProductController.create(req, res, next));
 
 export default app;
