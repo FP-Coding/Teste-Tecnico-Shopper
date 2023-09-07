853e54ab4df04f6ff5be4b80c7e9292e385fba8a
Felipe Pinto <felipe14062000@gmail.com>
2023-09-06 22:35:45 -0300

    feat: implementação das rotas get, put e delete de /product; e das rotas get e post de /pack

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,26 @@
-import express, { NextFunction, Request, Response } from 'express';
 import 'dotenv/config';
+import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
 import ProductController from './controllers/Product.controller';
+import PackController from './controllers/Pack.controller';
+import errorHandler from './middlewares/ErrorHandler';
 
 const app = express();
 
 app.use(express.json());
 
+app.put('/product/:id', (req: Request, res: Response, next: NextFunction) => ProductController.update(req, res, next));
+app.delete('/product/:id', (req: Request, res: Response, next: NextFunction) => ProductController.delete(req, res, next)); 
 app.get('/product/:id', (req: Request, res: Response, next: NextFunction) => ProductController.find(req, res, next)); 
 app.get('/product', (req: Request, res: Response, next: NextFunction) => ProductController.list(req, res, next)); 
 app.post('/product', (req: Request, res: Response, next: NextFunction) => ProductController.create(req, res, next));
+
+
+app.get('/pack/:id', (req: Request, res: Response, next: NextFunction) => PackController.find(req, res, next));
+app.get('/pack', (req: Request, res: Response, next: NextFunction) => PackController.list(req, res, next));
+app.post('/pack', (req: Request, res: Response, next: NextFunction) => PackController.create(req, res, next));
+
+app.use(errorHandler);
+
 
 export default app;
