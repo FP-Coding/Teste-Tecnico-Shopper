d88648072681951f35fec7e5dcf28602a76ed7bb
Felipe Pinto <felipe14062000@gmail.com>
2023-09-06 22:30:41 -0300

    fix: correção de declaração da ModelProduct

--- backend/src/database/mysql/models/Product.model.ts
+++ backend/src/database/mysql/models/Product.model.ts
@@ -2,7 +2,7 @@ import { Model, STRING, INTEGER ,DECIMAL } from 'sequelize';
 import db from '.';
 
 class Product extends Model {
-	declare readonly code: number;
+	declare code: number;
 	declare name: string;
 	declare costPrice: number;
 	declare salesPrice: number;
