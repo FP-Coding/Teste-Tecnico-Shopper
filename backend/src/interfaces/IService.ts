af848682462fe2ad29bbae7aa6431b5503e69716
Felipe Pinto <felipe14062000@gmail.com>
2023-09-07 19:42:40 -0300

    feat: adaptação de retorno das funções find e update

--- backend/src/interfaces/IService.ts
+++ backend/src/interfaces/IService.ts
@@ -1,11 +1,13 @@
+import { IProductUpdated } from './IProduct';
+
 export interface SimpleService<T> {
   create(obj: T): Promise<T>;
   list(): Promise<T[]>;
-  find(id: number): Promise<T | null>;
+  find(id: number): Promise<T[] | T | null>;
 }
 
 interface Service<T> extends SimpleService<T> {
-  update(id: number, obj: Partial<T>): Promise<boolean>;
+  update(id: number, obj: Partial<T>): Promise<boolean | IProductUpdated>;
   delete(id: number): Promise<boolean>;
 }
 
