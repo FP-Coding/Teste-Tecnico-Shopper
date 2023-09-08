78a44a95f3a51c5b476dab05d9948d8f0101a6e6
Felipe Pinto <felipe14062000@gmail.com>
2023-09-07 22:34:14 -0300

    feat: criação das funções de requisição a api com o axios

---
+++ frontend/src/utils/axios.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const PORT = 3001;
+const api = axios.create({ baseURL: `http://localhost:${process.env.REACT_APP_BACKEND_PORT || PORT}` });
+
+export const postRequest = async <T>(endpoint: string, body: T) => {
+  const { data } = await api.post(endpoint, body);
+  return data;
+};
+
+export const getRequest = async (endpoint: string) => {
+  const { data } = await api.post(endpoint);
+  return data;
+};
+
+export const patchRequest = async <T>(endpoint: string, body: T) => {
+  const { data } = await api.patch(endpoint, body);
+  return data;
+}
+
+export default api;
