e3b6515d62e31c133efca22b4151975e5159bd54
Felipe Pinto <felipe14062000@gmail.com>
2023-09-08 09:56:49 -0300

    feat: implementação da page Product

--- frontend/src/pages/Products.tsx
+++ frontend/src/pages/Products.tsx
@@ -1,9 +1,43 @@
+import { useEffect, useState } from "react";
+import { getRequest } from "../utils/axios";
+import { IProduct } from "../interfaces/IProduct";
+import ProductCard from "../components/ProductCard";
+
 function Products() {
+  const [dataProducts, setDataProducts] = useState([]);
+
+  useEffect(() => {
+    const request = async () => {
+      const response = await getRequest('/products');
+      const formatProducts = response.map(({ salesPrice, costPrice, ...rest }: IProduct) => {
+        return {
+          ...rest,
+          salesPrice: Number(salesPrice),
+          costPrice: Number(costPrice)
+        };
+      });
+      setDataProducts(formatProducts);
+    };
+    request();
+  }, []);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <h1>Products</h1>
+      <div>
+        { dataProducts.map(({code, name, salesPrice, costPrice}: IProduct) => {
+            return (
+              <ProductCard 
+                code={code} 
+                name={name} 
+                salesPrice={salesPrice} 
+                costPrice={costPrice} 
+              />
+            );
+        }) }
+      </div>
+    </div>
+  );
 }
 
-export default Products
+export default Products;
