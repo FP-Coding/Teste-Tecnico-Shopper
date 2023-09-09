9390ed36a63e10bd7ef1c51ef9f38a198b0c214b
Felipe Pinto <felipe14062000@gmail.com>
2023-09-08 21:03:40 -0300

    feat: criação das interfaces para product

--- frontend/src/interfaces/IProduct.ts
+++ frontend/src/interfaces/IProduct.ts
@@ -1,6 +1,29 @@
-export interface IProduct {
+import { IErrorMessage } from "./IErrorMessage";
+
+export interface IProductUpdate {
+  code: number,
+  newPrice: number,
+}
+
+export interface IProductCore {
   code: number,
   name: string,
+}
+
+export interface IProduct extends IProductCore {
   costPrice: number,
   salesPrice: number
+}
+
+export interface IProductUpdated extends IProductCore {
+  newSalesPrice: number | string,
+  pastSalesPrice: number
+  messageError: IErrorMessage[]
+}
+
+export interface IPacks {
+  id: number,
+  packId: number,
+  productId: number,
+  qty: number
 }
