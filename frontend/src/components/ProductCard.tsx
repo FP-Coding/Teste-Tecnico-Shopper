05ca06684008fda515c60df139fc20713c0d6ed6
Felipe Pinto <felipe14062000@gmail.com>
2023-09-08 21:03:23 -0300

    feat: criação dos card de product

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,12 +1,18 @@
 import { IProduct } from "../interfaces/IProduct";
 
-function ProductCard (data: IProduct) {
+function ProductCard ({ name, code, salesPrice }: IProduct) {
   return (
-    <div id={`${data.code}`} className="flex">
+    <div id={`${code}`} className="flex flex-col border-solid border-[1px] border-black w-1/4 text-center">
       <h2>
-        {data.name}
+        <span>
+          {name}  
+        </span>
+        {' - CÓDIGO '}
+        <span>
+          {code}
+        </span>
       </h2>
-      <p>{data.salesPrice}</p>
+      <p>R$ <span>{salesPrice}</span></p>
     </div>
   );
 }
